2aa6b795f60c7a523d360b872026a55f0fb0914d
Tim Davis <tim.hifi@gmail.com>
2018-07-04 13:48:12 -0500

    added some functions

--- little-schemer.lisp
+++ little-schemer.lisp
@@ -1,10 +1,14 @@
+(declaim (optimize #+sbcl (sb-c::merge-tail-calls 3) #+sbcl (sb-c::insert-debug-catch 0))) 
+
 ;; Number games
 (defun addtup (tup)
+  "Sums a tuple."
   (cond
     ((null tup) 0)
     ((+ (car tup) (addtup (cdr tup))))))
 
 (defun x (n m)
+  "Recursive multiplication of n by m."
   (cond
     ((zerop m) 0)
     ((+ n (x n (sub1 m))))))
@@ -14,6 +18,7 @@
   (- n 1))
 
 (defun tup+ (tup1 tup2)
+  "Adds two tuples together."
   (cond    
     ((null tup1) tup2)
     ((null tup2) tup1)
@@ -27,7 +32,43 @@
     ((my> (sub1 n) (sub1 m)))))
 
 (defun my< (n m)
+  "Is n less than m?"
   (cond
     ((zerop m) nil)
     ((zerop n) t)
     ((my< (sub1 n) (sub1 m)))))
+
+(defun my= (n m)
+  "Is n equal to m?"
+  (cond
+    ((zerop n) (zerop m))
+    ((zerop m) nil)
+    ((my= (sub1 n) (sub1 m)))))
+
+(defun pow (n m)
+  "n to m power."
+  (cond
+    ((zerop m) 1)
+    ((x n (pow n (sub1 m))))))
+
+(defun add1 (n)
+  "Increments n by 1."
+  (+ n 1))
+
+(defun quot (n m)
+  "Recursive division of n by m."
+  (cond
+    ((my< n m) 0)
+    ((add1 (quot (- n m) m)))))
+
+(defun mylen (lat)
+  "Returns the number of elements in lat."
+  (cond
+    ((null lat) 0)
+    ((add1 (mylen(cdr lat))))))
+
+(defun pick (n lat)
+  "Returns the nth element of lat."
+  (cond
+    ((zerop (sub1 n)) (car lat))
+    ((pick (sub1 n) (cdr lat)))))
